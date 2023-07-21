8ff6debb13573cc59059f1597dbcb92e35a2981f
Daniel Pape <>
2023-07-21 10:56:54 +0200

    refactor: use Operations trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,33 @@
-
 #[derive(Debug)]
 pub struct Vector {
     pub dimension: usize,
     pub entries: Vec<f64>,
 }
 
+pub trait Operations {
+    fn add(&self, other: &Self) -> Self;
+    fn scale(&self, factor: f64) -> Self;
+    fn dot(&self, other: &Vector) -> f64;
+}
+
 fn check_dimension(v: &Vector, w: &Vector) {
     if v.dimension != w.dimension {
-        panic!("Attempt to add vectors of different dimensions: {} vs {}.", v.dimension, w.dimension)
+        panic!(
+            "Attempt to add vectors of different dimensions: {} vs {}.",
+            v.dimension, w.dimension
+        )
     }
 }
 
-impl Vector {
-    pub fn zeros(dimension: usize) -> Self {
-        Vector {
-            dimension: dimension,
-            entries: vec![0.0; dimension]
-        }
-    }
-
-
-    pub fn new(entries: Vec<f64>) -> Self {
-        Vector {
-            dimension: entries.len(),
-            entries: entries,
-        }
-    }
-
-    pub fn add(&self, v: &Vector) -> Vector {
-        check_dimension(self, &v);
+impl Operations for Vector {
+    fn add(&self, other: &Self) -> Self {
+        check_dimension(self, &other);
 
         let added_entries: Vec<f64> = self
             .entries
             .iter()
-            .zip(v.entries.iter())
-            .map(|(x, y)| { x + y })
+            .zip(other.entries.iter())
+            .map(|(x, y)| x + y)
             .collect::<Vec<f64>>();
 
         Vector {
@@ -43,19 +36,39 @@ impl Vector {
         }
     }
 
-    pub fn scale(&self, factor: f64) -> Vector {
+    fn scale(&self, factor: f64) -> Vector {
         Vector {
             dimension: self.dimension.clone(),
-            entries: self.entries.iter().map(|x| { factor * x }).collect::<Vec<f64>>(),
+            entries: self
+                .entries
+                .iter()
+                .map(|x| factor * x)
+                .collect::<Vec<f64>>(),
         }
     }
 
-    pub fn dot(&self, other: &Vector) -> f64 {
+    fn dot(&self, other: &Vector) -> f64 {
         self.entries
             .iter()
             .zip(other.entries.iter())
-            .map(|(x, y)| { x * y })
+            .map(|(x, y)| x * y)
             .sum()
+    }
+}
+
+impl Vector {
+    pub fn zeros(dimension: usize) -> Self {
+        Vector {
+            dimension: dimension,
+            entries: vec![0.0; dimension],
+        }
+    }
+
+    pub fn new(entries: Vec<f64>) -> Self {
+        Vector {
+            dimension: entries.len(),
+            entries: entries,
+        }
     }
 }
 
--- tests/kmeans_test.rs
+++ tests/kmeans_test.rs
@@ -1,6 +1,6 @@
 use proptest::prelude::*;
-use core::cmp::min;
 use kmeans::Vector;
+use kmeans::Operations;
 mod custom_strategies;
 
 #[test]
